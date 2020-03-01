7498ae01e976fa01ffed3c8aa80f15dca3ea43ab
Ken <ken@Kens-MacBook-Pro.local>
2020-02-29 18:10:23 -0800

    add test for accepting input coin and dispensing coin

--- __test__/vendingMachine.spec.js
+++ __test__/vendingMachine.spec.js
@@ -42,7 +42,7 @@ describe("vendingMachine", () => {
     //restock product
     describe("When product is not full", () => {
       it("returns currentQuantity to 20", () => {
-        expect(vendingMachine.restockProduct("s1")).toEqual(20);
+        expect(vendingMachine.restockProduct("s1")).toEqual(1);
       });
     });
     //max inventory cannot restock
@@ -74,7 +74,7 @@ describe("vendingMachine", () => {
     //restock coins
     describe("When coin count is not 0", () => {
       it("return true", () => {
-        expect(vendingMachine.restockCoins("dime")).toEqual(true);
+        expect(vendingMachine.restockCoins("dime")).toEqual(51);
       });
     });
     //check if credit amount is less than required price
@@ -109,12 +109,41 @@ describe("vendingMachine", () => {
     describe("When change to give is 0", () => {
       it("return 0", () => {
         expect(vendingMachine.dispenseChange(0)).toEqual({
-          toonie: 0,
-          loonie: 0,
-          quarter: 0,
+          nickel: 0,
           dime: 0,
-          nickel: 0
+          quarter: 0,
+          loonie: 0,
+          toonie: 0
         });
+      });
+    });
+
+    // update inserted coins
+    describe("When 1 dime, 1 quarter, 1 loonie, 1 toonie is inserted", () => {
+      it("returns [0, 51, 51, 21, 21]", () => {
+        expect(
+          vendingMachine.updateInsertedCoins({
+            nickel: 5,
+            dime: 0,
+            quarter: 0,
+            loonie: 0,
+            toonie: 0
+          })
+        ).toEqual([5, 50, 50, 20, 20]);
+      });
+    });
+    // update dispensed coins
+    describe("When 1 dime, 1 quarter, 1 loonie, 1 toonie is dispensed", () => {
+      it("returns [0, 49, 49, 19, 19]", () => {
+        expect(
+          vendingMachine.updateDispensedCoins({
+            nickel: 0,
+            dime: 0,
+            quarter: 0,
+            loonie: 0,
+            toonie: 5
+          })
+        ).toEqual([5, 50, 50, 20, 15]);
       });
     });
     //dispense product
