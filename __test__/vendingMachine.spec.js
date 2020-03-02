285f7bde0fce96d2113209efb2b6c5d713941f37
Ken <ken@Kens-MacBook-Pro.local>
2020-03-01 20:11:30 -0800

    update test value for updateInsertedCoins and updateDispensedCoins

--- __test__/vendingMachine.spec.js
+++ __test__/vendingMachine.spec.js
@@ -12,7 +12,6 @@ describe("vendingMachine", () => {
       expect(vendingMachine.getInventory()).toEqual(inventory);
     });
   });
-  //item selection
   describe("When product s1 is selected", () => {
     it("return s1", () => {
       expect(vendingMachine.itemSelection("s1")).toEqual({
@@ -23,7 +22,6 @@ describe("vendingMachine", () => {
         maxQuantity: 20
       });
     });
-    //check if inventory is empty
     describe("When product s1 is empty", () => {
       it("return 0", () => {
         expect(() => {
@@ -31,7 +29,6 @@ describe("vendingMachine", () => {
         }).toThrow("Out of stock");
       });
     });
-    //check if inventory is low
     describe("When product s1 is low", () => {
       it("return 2", () => {
         expect(() => {
@@ -39,13 +36,11 @@ describe("vendingMachine", () => {
         }).toThrow("Stock is low, please refill");
       });
     });
-    //restock product
     describe("When product is not full", () => {
       it("returns currentQuantity to 20", () => {
         expect(vendingMachine.restockProduct("s1")).toEqual(1);
       });
     });
-    //max inventory cannot restock
     describe("When product is full", () => {
       it("returns currentQuantity to 20", () => {
         expect(() => {
@@ -53,31 +48,26 @@ describe("vendingMachine", () => {
         }).toThrow("Inventory maxed out");
       });
     });
-    //validate coin input amount
     describe("When coin input is [5, 5, 1, 1, 1]", () => {
       it("return 3.00", () => {
         expect(vendingMachine.coinInputAmount([5, 5, 1, 1, 1])).toEqual(4.0);
       });
     });
-    //check if coin type is available
     describe("When coin count is 0", () => {
       it("return false", () => {
         expect(vendingMachine.coinInventoryCheck("nickel")).toEqual(false);
       });
     });
-    //check if coin type is available
     describe("When coin count is not 0", () => {
       it("return true", () => {
         expect(vendingMachine.coinInventoryCheck("dime")).toEqual(true);
       });
     });
-    //restock coins
     describe("When coin count is not 0", () => {
       it("return true", () => {
         expect(vendingMachine.restockCoins("dime")).toEqual(51);
       });
     });
-    //check if credit amount is less than required price
     describe("When credit amount is less than price", () => {
       it("insufficient fund", () => {
         expect(() => {
@@ -85,7 +75,6 @@ describe("vendingMachine", () => {
         }).toThrow("insufficient funds");
       });
     });
-    //calculate change
     describe("When credit amount is greater or equal to price, calculate change", () => {
       it("return 0", () => {
         expect(vendingMachine.validatePurchase("s1", [0, 0, 2, 1, 0])).toEqual(
@@ -93,7 +82,6 @@ describe("vendingMachine", () => {
         );
       });
     });
-    //dispense change
     describe("calculate minimal amount of change to give", () => {
       it("return 2 toonie, 1 loonie, 2 quarter, 0 dime, 0 nickel", () => {
         expect(vendingMachine.dispenseChange(5.5)).toEqual({
@@ -105,7 +93,6 @@ describe("vendingMachine", () => {
         });
       });
     });
-    //no change to dispense
     describe("When change to give is 0", () => {
       it("return 0", () => {
         expect(vendingMachine.dispenseChange(0)).toEqual({
@@ -117,8 +104,6 @@ describe("vendingMachine", () => {
         });
       });
     });
-
-    // update inserted coins
     describe("When 1 nickel is inserted", () => {
       it("returns [5, 50, 50, 20, 20]", () => {
         expect(vendingMachine.updateInsertedCoins([5, 0, 0, 0, 0])).toEqual([
@@ -130,7 +115,6 @@ describe("vendingMachine", () => {
         ]);
       });
     });
-    // update dispensed coins
     describe("When 5 toonie is dispensed", () => {
       it("returns [5, 50, 50, 19, 20]", () => {
         expect(
@@ -144,7 +128,6 @@ describe("vendingMachine", () => {
         ).toEqual([5, 50, 50, 19, 20]);
       });
     });
-    //dispense product
     describe("When s3 is dispensed", () => {
       it("returns 19", () => {
         expect(vendingMachine.dispenseProduct("s3")).toEqual(19);
